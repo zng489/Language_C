f84c57d4a2912c402f109355d05d43d621017222
zhang.489@hotmail.com <zhang.489@hotmail.com>
2022-12-03 11:22:17 -0300

    language_C

---
+++ condition.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    int x = 41;
+    x =x+ 1;
+        if (x == 42) {
+            printf("You succeed!");}
+}
---
+++ variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   int z = 4350;
+   printf("%d",z);
+   return 0;
+}
+
+//
+
